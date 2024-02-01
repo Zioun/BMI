a5b6036a9d62b2d7b17a2342c0b0305b46287100
Zioun <jionkhan0@gmail.com>
2024-02-01 10:37:40 +0600

    added

--- bmi.js
+++ bmi.js
@@ -1,17 +1,48 @@
-let height = 4;
-let weight = 138;
+let height = 5;
+let weight = 204;
 if(height <= 4.10 && weight >= 90 && weight <= 115){
     console.log("Normal");
 }else if(height <= 4.10 && weight >= 116 && weight <= 138){
     console.log("OverWeight");
 }else if(height <= 4.10 && weight >= 139 && weight <= 186){
     console.log("Obese");
-}else if(height <= 4.10 && weight >= 187 && weight < 191){
+}else if(height <= 4.10 && weight >= 187){
     console.log("Extreme Obesity");
     // !-----------------one
-}
-else if(height <= 4.11 && weight >= 94 && weight <= 119){
+}else if(height <= 4.11 && weight >= 94 && weight <= 119){
     console.log("Normal");
 }else if(height <= 4.11 && weight >= 120 && weight <= 143){
     console.log("OverWeight");
+}else if(height <= 4.11 && weight >= 144 && weight <= 193){
+    console.log("Obese");
+}else if(height <= 4.10 && weight >= 194){
+    console.log("Extreme Obesity");
+    // !-----------------two
+}else if(height <= 5 && weight >= 97 && weight <= 123){
+    console.log("Normal");
+}else if(height <= 5 && weight >= 124 && weight <= 148){
+    console.log("OverWeight");
+}else if(height <= 5 && weight >= 149 && weight <= 199){
+    console.log("Obese");
+}else if(height <= 5 && weight >= 200){
+    console.log("Extreme Obesity");
+    // !-----------------three
+}else if(height <= 5.1 && weight >= 100 && weight <= 127){
+    console.log("Normal");
+}else if(height <= 5.1 && weight >= 128 && weight <= 153){
+    console.log("OverWeight");
+}else if(height <= 5.1 && weight >= 154 && weight <= 206){
+    console.log("Obese");
+}else if(height <= 5.1 && weight >= 207){
+    console.log("Extreme Obesity");
+    // !-----------------four
+}else if(height <= 5.2 && weight >= 104 && weight <= 131){
+    console.log("Normal");
+}else if(height <= 5.2 && weight >= 132 && weight <= 158){
+    console.log("OverWeight");
+}else if(height <= 5.2 && weight >= 159 && weight <= 213){
+    console.log("Obese");
+}else if(height <= 5.2 && weight >= 214){
+    console.log("Extreme Obesity");
+    // !-----------------five
 }
