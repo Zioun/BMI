d740c7b959bcae726988b4860c3c6414ad969239
Zioun <jionkhan0@gmail.com>
2024-02-01 09:59:27 +0600

    first commit

---
+++ bmi.js
@@ -0,0 +1,17 @@
+let height = 4;
+let weight = 138;
+if(height <= 4.10 && weight >= 90 && weight <= 115){
+    console.log("Normal");
+}else if(height <= 4.10 && weight >= 116 && weight <= 138){
+    console.log("OverWeight");
+}else if(height <= 4.10 && weight >= 139 && weight <= 186){
+    console.log("Obese");
+}else if(height <= 4.10 && weight >= 187 && weight < 191){
+    console.log("Extreme Obesity");
+    // !-----------------one
+}
+else if(height <= 4.11 && weight >= 94 && weight <= 119){
+    console.log("Normal");
+}else if(height <= 4.11 && weight >= 120 && weight <= 143){
+    console.log("OverWeight");
+}
